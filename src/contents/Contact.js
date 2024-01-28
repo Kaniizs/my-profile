60854893bdcc0a597f38fcfebd5091427270468b
Isaraa <isaraa.p@ku.th>
2024-01-28 20:57:32 +0700

    Final v2.1

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -11,8 +11,7 @@ class Contact extends Component {
                 <h1 className="subtopic">Contact Me !</h1>
                 <h2 className="desc">You can search for my social or click the icon below !</h2>
                 <Socialbox icon="fas fa-phone" info="+66 81 357 7509" />
-                <Socialbox icon="fas fa-envelope" info="mojizsz12@gmail.com (Work)" />
-                <Socialbox icon="fas fa-envelope" info="lunlaa2014@gmail.com (General)" />
+                <Socialbox icon="fas fa-envelope" info="mojizsz12@gmail.com" />
                 <Socialbox icon="fab fa-line" info="Meteoriteism" />
                 <Socialbox icon="fas fa-map-marker-alt" info="Bangkok, Thailand" />
                 <Social />
