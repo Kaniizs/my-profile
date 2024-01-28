892d7660e05e9d8349590b2a64b8d099ad9866ec
Isaraa <isaraa.p@ku.th>
2024-01-28 18:24:52 +0700

    Final v1

--- src/components/skillsbox.js
+++ src/components/skillsbox.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Skillsbox = () => {
-    const myskills = ['Python', 'JavaScript/TypeScript', 'java', 'C++', 'React', 'Node.js', 'MongoDB', 'MySQL', 'HTML', 'CSS', 'Git', 'GitHub', 'Vercel', 'Scrum', 'Agile'];
+    const myskills = ['Python', 'JavaScript/TypeScript', 'Java', 'C++', 'React', 'Node.js', 'MongoDB', 'MySQL', 'HTML', 'CSS', 'Git', 'GitHub', 'Vercel', 'Scrum', 'Agile'];
 
     return (
         <div className="condiv skills">
--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -9,17 +9,12 @@ class Contact extends Component {
         return (
             <div className="condiv contact">
                 <h1 className="subtopic">Contact Me !</h1>
+                <h2 className="desc">You can search for my social or click the icon below !</h2>
                 <Socialbox icon="fas fa-phone" info="+66 81 357 7509" />
                 <Socialbox icon="fas fa-envelope" info="mojizsz12@gmail.com (Work)" />
                 <Socialbox icon="fas fa-envelope" info="lunlaa2014@gmail.com (General)" />
                 <Socialbox icon="fab fa-line" info="Meteoriteism" />
                 <Socialbox icon="fas fa-map-marker-alt" info="Bangkok, Thailand" />
-                <div className="contact-info">
-                    {/* <p><i className="fas fa-envelope"></i> : mojizsz12@gmail.com</p>
-                    <p><i className="fas fa-phone"></i> : 0813577509</p>
-                    <p><i className="fab fa-line"></i> : Meteoriteism</p>
-                    <p><i className="fab fa-facebook"></i> : Isaraa Phadungprasertdkul</p> */}
-                </div>
                 <Social />
             </div>
         );
